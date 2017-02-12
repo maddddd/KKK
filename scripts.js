7f5700257b8dd6d3972aec217bff48d08c8872f7
Giedrius Jocius <giedrius393@gmail.com>
2017-02-12 17:55:33 +0200

    first commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function Login() {
+	return "<a>Prisijungėte kaip Vardenis Pavardenis!</a><a href=\"javascript:document.getElementById('login').innerHTML = Logout()\"><img src=\"images/logout.png\" width=\"25\" height=\"25\"></img><a>";
+}
+
+function Logout() {
+	return "<a>Jūs atsijungėte nuo sistemos<\a>";
+}
+
+
